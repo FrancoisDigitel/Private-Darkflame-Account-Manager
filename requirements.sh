25c13003dbe69fb47b80b1f6a2b67b5ee79232d2
Francois Durieu Digitel <durieu@digitelstudios.lu>
2021-12-16 23:20:06 +0100

    removed .txt and made a script

---
+++ requirements.sh
@@ -0,0 +1,21 @@
+apt update -y
+apt upgrade -y
+apt install bcrypt -y
+apt install cffi -y
+apt install click -y
+apt install colorama -y
+apt install dominate -y
+apt install Flask -y
+apt install Flask-Bootstrap -y
+apt install Flask-Login -y
+apt install Flask-SQLAlchemy -y
+apt install greenlet -y
+apt install itsdangerous -y
+apt install Jinja2 -y
+apt install MarkupSafe -y
+apt install pycparser -y
+apt install PyMySQL -y
+apt install six -y
+apt install SQLAlchemy -y
+apt install visitor -y
+apt install Werkzeug -y
